8097c6f31c19bff7e64bba696594b71a276e24fb
PrasadMallampati <107248009+prasadmallampati@users.noreply.github.com>
2024-07-06 23:32:20 +0530

    Greedy_Algorithm.py

--- Greedy_Algorithm.py
+++ Greedy_Algorithm.py
@@ -1,1 +1,39 @@
+# Greedy Algorithm
 
+# @author: prasad
+
+
+def gredy(coins,amount):
+    
+    count=0
+    
+    if coins==0:
+        
+        return -1
+    
+    if amount==0:
+        
+        return 0
+    
+    coins.sort(reverse=True)
+    
+    for coin in coins:
+        
+        count+= amount//coin
+        
+        amount%= coin
+        
+    if amount !=0:
+        
+        return -1
+        
+    return count
+
+coins=list(map(int,input('enter coins values = ').split()))
+
+amount=int(input('enter amount= '))
+
+result=gredy(coins, amount)
+
+if result != -1:
+    print(f'count of coins is ={result}' )
