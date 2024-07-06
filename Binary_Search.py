10f987777404afe269bdc5061359f8a4d2cd2d63
PrasadMallampati <107248009+prasadmallampati@users.noreply.github.com>
2024-07-06 23:09:54 +0530

    Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,1 +1,39 @@
+"""
+@author: prasad
+"""
+# Binary Search 
 
+def BinarySearch(arr,target):
+    
+    low=0
+    
+    high=len(arr)-1
+    
+    while low<=high:
+        
+        mid=(low+high)//2
+        
+        if arr[mid]==target:
+            
+            return mid
+        
+        elif arr[mid]<=target:
+            
+            low=mid+1
+            
+        else:
+            
+            high=mid-1
+            
+    return -1
+
+arr_input=list(map(int,input('enter values=').split()))
+
+target=int(input('enter target element='))
+
+result =BinarySearch(arr_input,target)
+
+if result !=-1:
+    print(f'{target} element found at {result} index')
+else:
+    print(f'{target} element not found')
